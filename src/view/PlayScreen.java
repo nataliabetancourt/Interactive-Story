d1372bb9403c5a63642352790587c2520a6446a0
Natalia Betancourt <nataliabetancourtm@gmail.com>
2021-04-14 10:53:46 -0500

    Parallax effect on window

--- src/view/PlayScreen.java
+++ src/view/PlayScreen.java
@@ -7,18 +7,28 @@ import processing.core.PImage;
 public class PlayScreen {
 	
 	private PApplet app;
-	private PImage background;
+	private PImage background, window;
+	private int parallax;
+
 	
 	public PlayScreen(PApplet app) {
 		this.app = app;
 		this.app = app;
 		
 		//Load image
-		background = app.loadImage("./data/playscreen.jpg");
+		background = app.loadImage("./data/playscreen.png");
+		window = app.loadImage("./data/window.jpg");
 		
 	}
 	
 	public void draw() {
+		//Parallax (window)
+		parallax = (app.mouseX - 1400)/15;
+		
+		app.imageMode(PConstants.CORNER);
+		app.image(window, parallax, 5, 1200, 700);
+		
+		//Play screen
 		app.imageMode(PConstants.CORNER);
 		app.image(background, 0, 0, 1200, 700);
 	}
